8f4420712422ba36b147b7066b1c8325fcefb012
Guilherme Santos <72032143+guilherme-dsantos@users.noreply.github.com>
2024-03-30 14:51:10 +0000

    Fix minor performance issues

--- src/atm/atm.rs
+++ src/atm/atm.rs
@@ -18,11 +18,11 @@ fn extract_public_key(file_path: &str) -> Result<RsaPublicKey,String> {
 
 }
 
-fn validate_balance(s: String){
+fn validate_balance(s: &str){
     let pattern = regex::Regex::new(r"^(0|[1-9][0-9]*)\.[0-9]{2}$").unwrap();
-    match pattern.is_match(&s) {
+    match pattern.is_match(s) {
         true => {
-            let value = f64::from_str(&s).unwrap_or_else(|_| {
+            let value = f64::from_str(s).unwrap_or_else(|_| {
                 eprintln!("Failed to parse balance as a float.");
                 exit(252);
             });
@@ -37,17 +37,17 @@ fn validate_balance(s: String){
     }
 }
 
-fn validate_ip_address(s: String){
+fn validate_ip_address(s: &str){
     println!("Validating IP: {}", &s);
     let pattern = regex::Regex::new(r"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$").unwrap();
-    if !pattern.is_match(&s){
+    if !pattern.is_match(s){
         println!("Regex pattern: {:?}", pattern);
         eprintln!("{}", &s);
         exit(252)
     }
 }
 
-fn validate_port(s: String){
+fn validate_port(s: &str){
     if let Ok(port) = s.parse::<u16>() { //u16 goes to 65535
         if port < 1024{
             eprintln!("Port number out of valid range (1024-65535).");
@@ -58,13 +58,13 @@ fn validate_port(s: String){
         exit(251);
     }
 }
-fn validate_account(account_name: String) {
+fn validate_account(account_name: &str) {
     /*if account_name == "." || account_name == ".." {
         println!("Special account name is valid: {}", account_name);
     } else {*/
         let valid_pattern = regex::Regex::new(r"^[_\-\.0-9a-z]+$").unwrap();
-        let is_valid_length = account_name.len() >= 1 && account_name.len() <= 122;
-        let is_valid_name = valid_pattern.is_match(&account_name);
+        let is_valid_length = !account_name.is_empty() && account_name.len() <= 122;
+        let is_valid_name = valid_pattern.is_match(account_name);
 
         if is_valid_length && is_valid_name {
             println!("Account name is valid: {}", account_name);
@@ -76,10 +76,10 @@ fn validate_account(account_name: String) {
 }
 
 
-fn validate_file_name(file_name: String){
+fn validate_file_name(file_name: &str){
     let valid_pattern = regex::Regex::new(r"^[_\-\.0-9a-z]+$").unwrap();
-    let is_valid_length = file_name.len() >= 1 && file_name.len() <= 127;
-    let is_valid_name = valid_pattern.is_match(&file_name);
+    let is_valid_length = !file_name.is_empty() && file_name.len() <= 127;
+    let is_valid_name = valid_pattern.is_match(file_name);
     let is_not_special = file_name != "." && file_name != "..";
     if !(is_valid_length && is_valid_name && is_not_special) {
         exit(277);
@@ -102,12 +102,12 @@ fn main() -> std::io::Result<()> {
     let (auth_file, ip_address, port, card_file, account, operation): (String, String, String, String, String, Operation) = match atm_parser::cli() {
         Ok(matches) => {
             let auth_file = matches.value_of("auth-file").unwrap_or("bank.auth").to_string(); //alterar para working directory
-            validate_file_name(auth_file.clone());
+            validate_file_name(&auth_file);
             let ip_address = matches.value_of("ip-address").unwrap_or("127.0.0.1").to_string();
-            validate_ip_address(ip_address.clone());
+            validate_ip_address(&ip_address);
 
             let port = matches.value_of("port").unwrap_or("3000").to_string();
-            validate_port(port.clone());
+            validate_port(&port);
 
             /*let account = matches.value_of("account").unwrap_or_else(|| {
                 exit(251);
@@ -117,10 +117,10 @@ fn main() -> std::io::Result<()> {
             let account = matches.value_of("account").unwrap_or_else(|| {
                 exit(251);
             }).to_string();
-            validate_account(account.clone());
+            validate_account(&account);
 
-            let card_file = matches.value_of("card-file").map(|s| s.to_string()).unwrap_or(format!("{}{}", &account, ".card"));
-            validate_file_name(card_file.clone());
+            let card_file = matches.value_of("card-file").map(|s| s.to_string()).unwrap_or(format!("{}{}", account, ".card"));
+            validate_file_name(&card_file);
 
             let operation = if matches.is_present("balance") {
                 Operation::Balance(matches.value_of("balance").unwrap().to_string())
@@ -141,9 +141,14 @@ fn main() -> std::io::Result<()> {
         }
     };
 
+    //Connect to the bank
+    let mut stream = TcpStream::connect(ip_address.to_owned() + &port).unwrap_or_else(|_| {
+        exit(254);
+    });
+
     match operation {
         Operation::Balance(balance) => {
-            validate_balance(balance);
+            validate_balance(&balance);
             create_card_file(&card_file);
         },
         Operation::Deposit(deposit) => {
@@ -171,10 +176,7 @@ fn main() -> std::io::Result<()> {
         } 
     };
 
-    //Connect to the bank
-    let mut stream = TcpStream::connect(ip_address.to_owned() + &port).unwrap_or_else(|_| {
-        exit(254);
-    });
+    
 
     println!("{:?}", extracted_public_key);
     println!("{} {} {} {} {}", auth_file, ip_address, port, card_file, account);
